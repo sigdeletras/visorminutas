3d32a81e3845e11aa8333536787632d0f4f4e72b
Patricio Soriano <pasoriano@gmail.com>
2017-08-17 11:34:48 +0200

    adding visorategua

---
+++ visorategua/data/exp_vertice.js
@@ -0,0 +1,34 @@
+var exp_vertice = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 1, "x": 361071, "y": 4179726 }, "geometry": { "type": "Point", "coordinates": [ -4.578188979732231, 37.752459959404113 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 2, "x": 361183, "y": 4179895 }, "geometry": { "type": "Point", "coordinates": [ -4.576950399691001, 37.753999621904818 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 3, "x": 361098, "y": 4180074 }, "geometry": { "type": "Point", "coordinates": [ -4.577949206775007, 37.755599452200315 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 4, "x": 361132, "y": 4180173 }, "geometry": { "type": "Point", "coordinates": [ -4.577582325579742, 37.756496582763063 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 5, "x": 361189, "y": 4180224 }, "geometry": { "type": "Point", "coordinates": [ -4.57694524666608, 37.756964738346511 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 6, "x": 361206, "y": 4180332 }, "geometry": { "type": "Point", "coordinates": [ -4.576772987920218, 37.757940372078963 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 7, "x": 361271, "y": 4180377 }, "geometry": { "type": "Point", "coordinates": [ -4.576043958243401, 37.75835567954195 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 8, "x": 361450, "y": 4180253 }, "geometry": { "type": "Point", "coordinates": [ -4.573988938567454, 37.757265624810366 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 9, "x": 361633, "y": 4180082 }, "geometry": { "type": "Point", "coordinates": [ -4.571879624817171, 37.755752679379484 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 10, "x": 361635, "y": 4179943 }, "geometry": { "type": "Point", "coordinates": [ -4.571830426284957, 37.754500624837753 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 11, "x": 361519, "y": 4179598 }, "geometry": { "type": "Point", "coordinates": [ -4.573080931620646, 37.751374689293648 ] } },
+{ "type": "Feature", "properties": { "entidad": "BIC", "norden": 12, "x": 361200, "y": 4179563 }, "geometry": { "type": "Point", "coordinates": [ -4.576693998362865, 37.751010967311046 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 13, "x": 360810, "y": 4179692 }, "geometry": { "type": "Point", "coordinates": [ -4.581144117575391, 37.752113923327848 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 14, "x": 360873, "y": 4180181 }, "geometry": { "type": "Point", "coordinates": [ -4.580522983495497, 37.756529271377055 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 15, "x": 361017, "y": 4180528 }, "geometry": { "type": "Point", "coordinates": [ -4.578955341541625, 37.759677556431008 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 16, "x": 360881, "y": 4180808 }, "geometry": { "type": "Point", "coordinates": [ -4.580552423963441, 37.762179584126535 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 17, "x": 361112, "y": 4181136 }, "geometry": { "type": "Point", "coordinates": [ -4.577993642960807, 37.765169913620902 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 18, "x": 361511, "y": 4180929 }, "geometry": { "type": "Point", "coordinates": [ -4.573425742767786, 37.763365461438589 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 19, "x": 361727, "y": 4180751 }, "geometry": { "type": "Point", "coordinates": [ -4.570940416678623, 37.761794436856015 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 20, "x": 361914, "y": 4180645 }, "geometry": { "type": "Point", "coordinates": [ -4.568798008501036, 37.760867678904788 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 21, "x": 362064, "y": 4180527 }, "geometry": { "type": "Point", "coordinates": [ -4.567073268277564, 37.759827179276051 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 22, "x": 362008, "y": 4180395 }, "geometry": { "type": "Point", "coordinates": [ -4.567683681375762, 37.758629434437914 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 23, "x": 362299, "y": 4179947 }, "geometry": { "type": "Point", "coordinates": [ -4.564296289123592, 37.754636939448609 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 24, "x": 362109, "y": 4179493 }, "geometry": { "type": "Point", "coordinates": [ -4.566366107939296, 37.750517849635031 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 25, "x": 361853, "y": 4179191 }, "geometry": { "type": "Point", "coordinates": [ -4.569213496276889, 37.747758243600529 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 26, "x": 361009, "y": 4179201 }, "geometry": { "type": "Point", "coordinates": [ -4.578791988328387, 37.747720419559926 ] } },
+{ "type": "Feature", "properties": { "entidad": "ENTORNO", "norden": 27, "x": 360837, "y": 4179380 }, "geometry": { "type": "Point", "coordinates": [ -4.580777922847874, 37.749306997566109 ] } }
+]
+}
